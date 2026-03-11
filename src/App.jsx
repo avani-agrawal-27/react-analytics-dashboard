421dfe7cb226a56223b6fadad8efb741ad1a0c0d
avani-agrawal-27 <avaniagrawal27@hotmail.com>
2026-03-11 12:02:30 +0530

    React dashboard project created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div style={{textAlign:"center",marginTop:"100px"}}>
+      <h1>React Analytics Dashboard</h1>
+      <p>My First Portfolio Project</p>
+    </div>
+  )
+}
+
+export default App
